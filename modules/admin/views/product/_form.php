6969cc936ff260ae3e1378b4b4cbb4ac67a9af7c
Franklin Paula Aguirre <franklin.paula@share.com.ec>
2017-02-09 23:23:39 -0500

    multiple file upload

--- modules/admin/views/product/_form.php
+++ modules/admin/views/product/_form.php
@@ -2,7 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
+use kartik\widgets\FileInput;
+use yii\helpers\Url;
 /* @var $this yii\web\View */
 /* @var $model app\models\Product */
 /* @var $form yii\widgets\ActiveForm */
@@ -31,6 +32,33 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'support')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'important')->dropDownList([ 'YES' => 'YES', 'NO' => 'NO', ], ['prompt' => '']) ?>
+    <?= 
+FileInput::widget([
+    'name' => 'attachment_48[]',
+    'options'=>[
+        'multiple'=>true
+    ],
+   'pluginOptions' => [
+         'uploadUrl' => Url::to(['/site/file-upload']),
+        'uploadExtraData' => [
+            'product_id' => $model->id
+        ],
+        'initialPreview'=>[
+            "http://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/FullMoon2010.jpg/631px-FullMoon2010.jpg",
+            "http://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Earth_Eastern_Hemisphere.jpg/600px-Earth_Eastern_Hemisphere.jpg"
+        ],
+        'initialPreviewAsData'=>true,
+        'initialCaption'=>"Imágenes del Producto",
+        'initialPreviewConfig' => [
+            ['caption' => 'Moon.jpg', 'size' => '873727'],
+            ['caption' => 'Earth.jpg', 'size' => '1287883'],
+        ],
+        'overwriteInitial'=>false,
+        'maxFileSize'=>2800
+    ]
+]);
+
+    ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
