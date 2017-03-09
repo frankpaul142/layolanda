c0ee26d9dce94f42668633fbf40e28ed71fa8eee
Franklin Paula <frankpaul142@gmail.com>
2017-03-09 18:23:36 -0500

    local connection

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=layolanda',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=layolanda',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
